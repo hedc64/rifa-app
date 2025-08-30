192cab71b7bff92f79053a2a72837ad5dc6fbd69
hernan <hedc64@gmail.com>
2025-08-29 21:21:10 -0500

    actualiza whatsapp

--- public/js/main.js
+++ public/js/main.js
@@ -172,7 +172,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
           const total = selectedNumbers.length * 2000;
           const mensaje = `Hola, quiero pagar $${total} COP por ${selectedNumbers.length} números. Mis datos son: ${buyerName}, ${buyerPhone}, ${buyerId}. Hare el pago por Nequi y adjuntaré el comprobante.`;
-          const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(mensaje)}`;
+          whatsappLink.href = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(mensaje)}`;
           //window.location.href = whatsappLink;
           
           selectedNumbers = [];
