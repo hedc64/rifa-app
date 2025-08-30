b871caf82c32db6d8a1133f4f5ccda46ab3b65cf
hernan <hedc64@gmail.com>
2025-08-29 21:54:59 -0500

    actualiza agregar numeros mensaje

--- public/js/main.js
+++ public/js/main.js
@@ -167,13 +167,10 @@ document.addEventListener('DOMContentLoaded', () => {
           confirmation.style.display = 'none';
           paymentInfo.style.display = 'block';
 
-          //const message = `Hola, deseo comprar los números: ${selectedNumbers.join(', ')}. Mis datos son: ${buyerName}, ${buyerPhone}, ${buyerId}. Realizaré pago por Nequi y adjuntaré comprobante.`;
-          //whatsappLink.href = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-
           const total = selectedNumbers.length * 2000;
-          const mensaje = `Hola, quiero pagar $${total} COP por ${selectedNumbers.length} números. Mis datos son: ${buyerName}, ${buyerPhone}, ${buyerId}. Hare el pago por Nequi y adjuntaré el comprobante.`;
+          const numerosTexto = selectedNumbers.join(', '); // convierte [12, 34, 56] → "12, 34, 56"
+          const mensaje = `Hola, quiero pagar $${total} COP por ${selectedNumbers.length} números (${numerosTexto}). Mis datos son: ${buyerName}, ${buyerPhone}, ${buyerId}. Haré el pago por Nequi y adjuntaré el comprobante.`;
           whatsappLink.href = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(mensaje)}`;
-          //window.location.href = whatsappLink;
           
           selectedNumbers = [];
           acceptTermsCheckbox.checked = false;
